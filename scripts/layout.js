b36ba95e23726c6eedc70bf1d7eed90e966dfdda
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-01-08 00:22:40 -0800

    Add broken nav active fn

--- scripts/layout.js
+++ scripts/layout.js
@@ -15,7 +15,41 @@ async function loadFragment(id, url) {
     }
 }
 
-loadFragment("site-header", "/header.html")
-loadFragment("site-footer", "/footer.html")
+
+
+
 // Sets nav tab to active depending on page
 
+const pageNavItemIdDictionary = {
+    "Home": "nav-home",
+    "Our Doctors": "nav-doctors",
+    "Services Offered": "nav-services",
+    "Specials": "nav-specials",
+    "Insurances": "nav-insurances",
+    "Patient Resources": "nav-resources",
+    "Contact Us": "nav-contact"
+}
+
+function underlineActiveNavElement() {
+    const title = document.title;
+    for (const key in pageNavItemIdDictionary) {
+        if (title.includes(key)) {
+            const navItem = document.getElementById(pageNavItemIdDictionary[key]);
+            if (navItem) {
+                navItem.classList.add("active");
+            }
+
+        }
+    }
+}
+
+
+
+async function createLayout() {
+    loadFragment("site-header", "/header.html");
+    loadFragment("site-footer", "/footer.html");
+    const home = 
+    underlineActiveNavElement();
+}
+
+createLayout();
